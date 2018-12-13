e48579fa7f7105ddcffce1371c16ba44d42c0cdd
mrkamiali <kamiali63@gmail.com>
2018-12-13 14:59:03 +0500

    flutter...

---
+++ MyApp/my_app/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+//          child: Text('Hello World'),
+          child: Center(
+            child: Center(child: RandomWords()),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(WordPair.random().asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
